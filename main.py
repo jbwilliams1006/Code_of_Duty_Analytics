d5917ccdd8d74a43260f8c580aaa453e7df9df24
“Josh” <jbrockwilliams@gmail.com>
2023-02-28 12:29:21 -0600

    First row done

--- main.py
+++ main.py
@@ -15,12 +15,20 @@ with open('style.css') as f:
 
 
 # Row A
-a1, a2 = st.columns([1,2])
+a1, a2 = st.columns([1,3])
+# a1, a2,a3,a4= st.columns([3,1,1,1])
 a1.image(Image.open('screenshot.png'))
+a1.caption(f"Congratulations Joshua,")
+a1.text(f'Patients are seeing a 52.6% increase in mental health')
 a2.header(f'Week Overview')
-a2.metric(f"Patient Return", "72 out of 93", "+72%")
-a2.metric(f"Patient Graduation", "27, last week 30", "-3%")
-a2.metric(f"Costs", "850.97", "+92.4", "inverse")
+a3,a4,a5 = a2.columns(3
+                   )
+with a3:
+    st.metric(f"Patient Return", "72 out of 93", "+72%")
+with a4:
+    st.metric(f"Patient Graduation", "27, last week 30", "-3%")
+with a5:
+    st.metric(f"Costs", "850.97", "+92.4", "inverse")
 
 # Row B
 b1, b2, b3, b4 = st.columns(4)
