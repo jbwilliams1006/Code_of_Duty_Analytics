72ccdebed818ac3eaa578b3bc1c8eee5aa9eb227
“Josh” <jbrockwilliams@gmail.com>
2023-03-01 15:37:13 -0600

    Addition of third row, (All with mock Data)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-
+import plost
 
 # Page setting
 st.set_page_config(layout="wide")
@@ -11,16 +11,36 @@ st.set_page_config(layout="wide")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# Data
+# """
+ 
+#              ______           _          
+#     _  _    |_   _ `.        / |_        
+#   _| || |_    | | `. \ ,--. `| |-',--.   
+#  |_| || |_|   | |  | |`'_\ : | | `'_\ :  
+#  |_| || |_|  _| |_.' /// | |,| |,// | |, 
+#    |_||_|   |______.' \'-;__/\__/\'-;__/ 
+                                         
+ 
+# """
 MentalData = np.random.randn(10, 1)
 PhysicalData = np.random.randn(10, 1)
 SpiritualData = np.random.randn(10, 1)
 CrimeData = np.random.randn(10, 1)
-# seattle_weather = pd.read_csv('weather.csv', parse_dates=['date'])
-# stocks = pd.read_csv('stocks.csv')
+seattle_weather = pd.read_csv('weather.csv', parse_dates=['date'])
+stocks = pd.read_csv('stocks.csv')
 
 
-# Row A
+# """
+ 
+#              _______                              _       
+#     _  _    |_   __ \                            / \      
+#   _| || |_    | |__) |   .--.   _   _   __      / _ \     
+#  |_| || |_|   |  __ /  / .'`\ \[ \ [ \ [  ]    / ___ \    
+#  |_| || |_|  _| |  \ \_| \__. | \ \/\ \/ /   _/ /   \ \_  
+#    |_||_|   |____| |___|'.__.'   \__/\__/   |____| |____| 
+                                                          
+ 
+# """
 a1, a2 = st.columns([1,3])
 # a1, a2,a3,a4= st.columns([3,1,1,1])
 a1.image(Image.open('screenshot.png'))
@@ -36,6 +56,16 @@ with a4:
 with a5:
     st.metric(f"Costs", "850.97", "+92.4", "inverse")
 
+# """
+ 
+#              _______                         ______    
+#     _  _    |_   __ \                       |_   _ \   
+#   _| || |_    | |__) |   .--.   _   _   __    | |_) |  
+#  |_| || |_|   |  __ /  / .'`\ \[ \ [ \ [  ]   |  __'.  
+#  |_| || |_|  _| |  \ \_| \__. | \ \/\ \/ /   _| |__) | 
+#    |_||_|   |____| |___|'.__.'   \__/\__/   |_______/  
+                                                       
+ 
 # """
 b1, b2, b3, b4 = st.columns([2,2,2,2])
 with b1:
@@ -53,3 +83,33 @@ with b4:
     st.line_chart(CrimeData)
 
 
+# """
+ 
+#              _______                           ______  
+#     _  _    |_   __ \                        .' ___  | 
+#   _| || |_    | |__) |   .--.   _   _   __  / .'   \_| 
+#  |_| || |_|   |  __ /  / .'`\ \[ \ [ \ [  ] | |        
+#  |_| || |_|  _| |  \ \_| \__. | \ \/\ \/ /  \ `.___.'\ 
+#    |_||_|   |____| |___|'.__.'   \__/\__/    `.____ .' 
+                                                       
+ 
+# """
+
+# Row C
+c1, c2 = st.columns((7,3))
+with c1:
+    st.markdown('### Heatmap')
+    plost.time_hist(
+    data=seattle_weather,
+    date='date',
+    x_unit='week',
+    y_unit='day',
+    color='temp_max',
+    aggregate='median',
+    legend=None)
+with c2:
+    st.markdown('### Bar chart')
+    plost.donut_chart(
+        data=stocks,
+        theta='q2',
+        color='company')
