8eccbc6619fb2980e9350f663a9bbe9831644d3e
NoahOrta22 <noahraulscout@gmail.com>
2023-03-11 20:37:41 -0600

    Update signup/create hash class/create regUser fil

--- pages/Sign_Up.py
+++ pages/Sign_Up.py
@@ -1,9 +1,10 @@
 import json
 import streamlit as st
-import streamlit_authenticator as stauth
+# import streamlit_authenticator as stauth
 from google.oauth2 import service_account
 from google.cloud import firestore
 from page_navigation import nav_page
+from register_user import *
 
 
 ## to hide the sidebar
@@ -26,38 +27,52 @@ creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="care-of-duty-analytics")
 
 ## reference to the users collection ID
-users_ref = db.collection("users")
-preauth_users_ref = db.collection("preauthorized").document("emails")
-preauth_users = preauth_users_ref.get()
-preauth_user_emails = preauth_users.to_dict()["email"]
-
+users_ref = db.collection("credentials").document("usernames")
+users = users_ref.get()
 
 # #Creation of credentials dictionary
-creds = {"usernames":{}}
+creds = {"usernames": users.to_dict()}
 
+# reference to preauthorized emails 
+pre_auth_ref = db.collection("preauthorized").document("emails")
+pre_auth = pre_auth_ref.get()
 
-# get data from database into lists
-usernames = [user.id for user in users_ref.stream()]
-passwords = [user.to_dict()['hash_password'] for user in users_ref.stream()]
-names = [user.to_dict()['name'] for user in users_ref.stream()]
+# Creation of preauthorized emails
+auth_emails = {"emails": pre_auth.to_dict()}
 
-# turn lists into database
-for username, password, name in zip(usernames, passwords, names):
-    user_dict = {"name":name,"password":password}
-    creds["usernames"].update({username:user_dict})
+# ## reference to the users collection ID
+# users_ref = db.collection("users")
+# preauth_users_ref = db.collection("preauthorized").document("emails")
+# preauth_users = preauth_users_ref.get()
+# preauth_user_emails = preauth_users.to_dict()["email"]
 
+# # #Creation of credentials dictionary
+# creds = {"usernames":{}}
 
-#Creation of authentication object
-authenticator = stauth.Authenticate(creds,
-       "analytics_dashboard", "abcdef", cookie_expiry_days = 1, preauthorized=preauth_user_emails)
+# # get data from database into lists
+# usernames = [user.id for user in users_ref.stream()]
+# passwords = [user.to_dict()['hash_password'] for user in users_ref.stream()]
+# names = [user.to_dict()['name'] for user in users_ref.stream()]
+
+# # turn lists into database
+# for username, password, name in zip(usernames, passwords, names):
+#     user_dict = {"name":name,"password":password}
+#     creds["usernames"].update({username:user_dict})
+
+# #Creation of authentication object
+# authenticator = stauth.Authenticate(creds,
+#        "analytics_dashboard", "abcdef", cookie_expiry_days = 1, preauthorized=auth_emails)
 
 #######################################################################################
 
 try:
-    if authenticator.register_user('Register user', preauthorization=False):
+    if register_user('Register user', preauthorization=False, credentials=creds, preauthorized=auth_emails):
         st.success('User registered successfully')
         db.collection("credentials").document("usernames").set(
             creds['usernames'], merge = True
+        )
+        db.collection("preauthorized").document("emails").set(
+            auth_emails['emails']
         )
 except Exception as e:
     st.error(e)
---
+++ register_user.py
@@ -0,0 +1,107 @@
+import jwt
+import bcrypt
+import streamlit as st
+from datetime import datetime, timedelta
+import extra_streamlit_components as stx
+
+from hasher import Hasher
+
+def register_credentials(username: str, name: str, fname: str, lname: str, password: str, email: str, militaryID: str, 
+                            preauthorization: bool, credentials: dict, preauthorized: list):
+        """
+        Adds to credentials dictionary the new user's information.
+        Parameters
+        ----------
+        username: str
+            The username of the new user.
+        name: str
+            The name of the new user.
+        password: str
+            The password of the new user.
+        email: str
+            The email of the new user.
+        preauthorization: bool
+            The preauthorization requirement, True: user must be preauthorized to register, 
+            False: any user can register.
+        """
+        credentials['usernames'][username] = {'name': name, 'fname': fname, 'lname': lname, 'militaryid': militaryID,
+            'password': Hasher([password]).generate()[0], 'email': email}
+        if preauthorization:
+            preauthorized['emails'].remove(email)
+
+        return credentials, preauthorized
+
+
+class RegisterError(Exception):
+    """
+    Exceptions raised for the register user widget.
+    Attributes
+    ----------
+    message: str
+        The custom error message to display.
+    """
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+def register_user(form_name: str, location: str='main', preauthorization=True, credentials: list=[], preauthorized: list=[]) -> bool:
+        """
+        Creates a password reset widget.
+        Parameters
+        ----------
+        form_name: str
+            The rendered name of the password reset form.
+        location: str
+            The location of the password reset form i.e. main or sidebar.
+        preauthorization: bool
+            The preauthorization requirement, True: user must be preauthorized to register, 
+            False: any user can register.
+        Returns
+        -------
+        bool
+            The status of registering the new user, True: user registered successfully.
+        """
+        if preauthorization:
+            if not preauthorized:
+                raise ValueError("preauthorization argument must not be None")
+        if location not in ['main', 'sidebar']:
+            raise ValueError("Location must be one of 'main' or 'sidebar'")
+        if location == 'main':
+            register_user_form = st.form('Register user')
+        elif location == 'sidebar':
+            register_user_form = st.sidebar.form('Register user')
+
+        register_user_form.subheader(form_name)
+        new_email = register_user_form.text_input('Email')
+        new_username = register_user_form.text_input('Username').lower()
+        new_fname = register_user_form.text_input('First Name')
+        new_lname = register_user_form.text_input('Last Name')
+        new_militaryID = register_user_form.text_input('Military ID')
+        new_password = register_user_form.text_input('Password', type='password')
+        new_password_repeat = register_user_form.text_input('Repeat password', type='password')
+        new_name = new_fname + " " + new_lname
+        
+
+        if register_user_form.form_submit_button('Register'):
+            if len(new_email) and len(new_username) and len(new_fname) and len(new_lname) and len(new_militaryID) and len(new_password) > 0:
+                if new_username not in credentials['usernames']:
+                    if new_password == new_password_repeat:
+                        if preauthorization:
+                            if new_email in preauthorized['emails']:
+                                credentials, preauthorized = register_credentials(new_username, new_name, new_fname, new_lname, new_password, 
+                                                                                  new_email, new_militaryID, preauthorization, credentials, preauthorized)
+                                return True
+                            else:
+                                raise RegisterError('User not preauthorized to register')
+                        else:
+                            credentials, preauthorized = register_credentials(new_username, new_name, new_fname, new_lname, new_password, 
+                                                                                  new_email, new_militaryID, preauthorization, credentials, preauthorized)
+                            return True
+                    else:
+                        raise RegisterError('Passwords do not match')
+                else:
+                    raise RegisterError('Username already taken')
+            else:
+                raise RegisterError('Please enter an email, username, first name, last name, military ID and password')
+
