71653f894dce4db0fcd7a47b5db590bd82a30228
Leslie-N-Cook <leslienichole.co@gmail.com>
2023-04-11 09:16:16 -0500

    added pages

---
+++ pages/_Support Statistics.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from Dashboard import authenticator
+from VisCourses import VisCourses
+from PIL import Image
+
+st.set_page_config(layout="wide")
+# Chanegs the font size using custom css
+st.markdown("""
+<style>
+.big-font {
+    font-size:100px !important;
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: bold;
+}
+
+</style>
+""", unsafe_allow_html=True)
+st.subheader(":green[SAFB Support Services Statistics]")
+st.text("Personnel completing additional support classes offered at Shepard Air Force Base.")
+
+VisCourses.DVLine()
+VisCourses.SALine()
+VisCourses.alcLine()
+VisCourses.angerLine()
+VisCourses.stressLine()
+
+logo = Image.open('Icons/fullName_logo.png')
+st.sidebar.image(logo, use_column_width =True)
+st.sidebar.success("Select a page above.")
+#Logout button
+authenticator.logout("Logout", "sidebar")
